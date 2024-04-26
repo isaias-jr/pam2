879b3980c09f2bf7955ea30d43d391ad9234ac15
Isaias <juniorlilineto@gmail.com>
2024-04-25 22:46:43 -0300

    comunicacao com viacep

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,13 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+function consultacep()
+{
+ let requisao = new  XMLHttpRequest();
+ let url = "https://viacep.com.br/ws/17055210/json/";
+ requisao.open("GET", url);
+ requisao.send();
+ requisao.onload = function (){
+    alert(JSON.parse(requisao.response).logradouro);
+ }
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
 }
+let botao = document.querySelector("#botao");
+ botao.addEventListener("click", consultacep);
