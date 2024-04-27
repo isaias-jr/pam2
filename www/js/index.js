26b5fdbb4754f344a175a320c0d2c61871c87478
Isaias jr <juniorlilineto@gmail>
2024-04-26 22:39:33 -0300

    logradouro

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,18 @@
 function consultacep()
 {
+ let campo = document.querySelector("#cep")  
  let requisao = new  XMLHttpRequest();
- let url = "https://viacep.com.br/ws/17055210/json/";
+ let url = "https://viacep.com.br/ws/" + campo.value + "/json/";
  requisao.open("GET", url);
  requisao.send();
  requisao.onload = function (){
-    alert(JSON.parse(requisao.response).logradouro);
+   let objetocep = JSON.parse(requisao.response)
+   let campoEndereco = document.querySelector("#logradouro")
+   let campoLocalidade = document.querySelector("#localidade")
+   let campoBairro = document.querySelector("#bairro")
+   campoEndereco.value = objetocep.logradouro
+   campoLocalidade.value = objetocep.localidade
+   campoBairro.value = objetocep.bairro
  }
 
 }
